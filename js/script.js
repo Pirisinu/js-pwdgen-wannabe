a6f6fa88b0381d800e90f980deb8c1e0a4cddeeb
Pirisinu <palazzialessandro2@gmail.com>
2023-09-06 14:54:05 +0200

    [ADD]JS H1 function to user interaction

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/* TITLE */
+const outputTitle = document.getElementById('heading');
+
+const name = prompt('Inserire il nome');
+const lastname = prompt('Inserire il cognome');
+const favouriteColor = prompt('Inserire il colore preferito');
+
+const titleMessage =
+`Ciao ${name} ${lastname}. <br/>
+Bel colore il ${favouriteColor}`;
+
+outputTitle.innerHTML = titleMessage;
+/* PASSWORD GENERATED */
+const outputPsw = document.getElementById('pswgen');
